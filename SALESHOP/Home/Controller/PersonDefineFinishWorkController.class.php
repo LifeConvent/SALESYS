5924e8a16d5ef4f07978ad528ef69dfb96d09d9a
newchinalife <a179703233@163.com>
2018-11-12 13:02:23 +0800

    修复灌数锁定问题

--- SALESHOP/Home/Controller/PersonDefineFinishWorkController.class.php
+++ SALESHOP/Home/Controller/PersonDefineFinishWorkController.class.php
@@ -106,6 +106,20 @@ class PersonDefineFinishWorkController extends Controller
         $queryDateStart = I('get.queryDateStart');
         $queryDateEnd = I('get.queryDateEnd');
         $method = new MethodController();
+        ##############################################################  公共JS处理部分  ############################################################################
+        //JS请求公共处理部分 TRUE锁定
+//        if($method->publicCheck()==1){
+//            $result['status'] = "failed";
+//            $result['lock'] = "true";
+//            $result['message'] = "您的用户已被锁定，已无法使用本系统，如有疑问请联系管理员确认！";
+//            exit(json_encode($result));
+//        }else if($method->publicCheck()==2){
+//            $result['status'] = "failed";
+//            $result['lock'] = "false";
+//            $result['message'] = "管理员正在后台进行灌数，暂时无法刷新系统，如有疑问请联系管理员确认！";
+//            exit(json_encode($result));
+//        }
+        ############################################################################################################################################################
         $conn = $method->OracleOldDBCon();
         //获取用户权限类型-1-管理员2-机构组长3-个人
         $userType = $method->getUserType();
