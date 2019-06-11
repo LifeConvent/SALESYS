9b329c732da94329fad024fe27e73bcce63db9b6
NCLNTDOMAIN\gaobiao <gaobiao@newchinalife.com>
2019-06-11 15:27:30 +0800

    修改关键指标查询

--- SALESHOP/Home/Controller/DayPostController.class.php
+++ SALESHOP/Home/Controller/DayPostController.class.php
@@ -340,6 +340,29 @@ class DayPostController extends Controller
             $sql_fix = " AND INSERT_DATE = TRUNC(TO_DATE('$queryDate','YYYY-MM-DD')) ";
         }
         if((int)$type==1){
+            $select_nbuw = "SELECT B.ORDER_LIST,C.EXE_NAME,C.BUSI_TYPE_NAME AS EXE_TYPE,A.* 
+                                  FROM HXBX_KPI A 
+                                  LEFT JOIN TMP_CODE_MAP B 
+                                       ON A.BUSI_TYPE = B.CODE_TYPE 
+                                  LEFT JOIN HXBX_KPI_CODES C
+                                    ON A.BUSI_TYPE = C.BUSI_TYPE
+                                WHERE A.BUSI_TYPE LIKE '%$BxOrganCode%' ".$sql_fix."
+                                ORDER BY B.ORDER_LIST";
+            $result_rows = oci_parse($conn, $select_nbuw); // 配置SQL语句，执行SQL
+            $result_all = $method->search_long($result_rows);
+            Log::write($username.' 关键指标数据库查询SQL：'.$select_nbuw,'INFO');
+            foreach($result_all AS $exe){
+                $result[$exe['ORDER_LIST']]['ZB_NAME'] = $exe['EXE_NAME'];
+                $result[$exe['ORDER_LIST']]['ZB_TYPE'] = $exe['EXE_TYPE'];
+                $result[$exe['ORDER_LIST']]['NUM_OLD_SUM'] = $exe['OLD_NUM'];
+                $result[$exe['ORDER_LIST']]['NUM_NEW_SUM'] = $exe['NEW_NUM'];
+                $result[$exe['ORDER_LIST']]['NUM_DIFF'] = $exe['NUM_DIFF'];
+                $result[$exe['ORDER_LIST']]['NUM_SAME_RADIO'] = $exe['NUM_SAMERATE'];
+                $result[$exe['ORDER_LIST']]['FEE_OLD_SUM'] = $exe['OLD_AMOUNT'];
+                $result[$exe['ORDER_LIST']]['FEE_NEW_SUM'] = $exe['NEW_AMOUNT'];
+                $result[$exe['ORDER_LIST']]['FEE_DIFF'] = $exe['AMOUNT_DIFF'];
+                $result[$exe['ORDER_LIST']]['FEE_SAME_RADIO'] = $exe['AMOUNT_SAMERATE'];
+            }
             $select_nbuw = "SELECT B.ORDER_LIST,A.* 
                                   FROM TMP_DAYPOST_BX_EXE A 
                                   LEFT JOIN TMP_CODE_MAP B 
@@ -361,12 +384,26 @@ class DayPostController extends Controller
                 $result[$exe['ORDER_LIST']]['FEE_DIFF'] = $exe['FEE_DIFF'];
                 $result[$exe['ORDER_LIST']]['FEE_SAME_RADIO'] = $exe['FEE_SAME_RADIO'];
             }
-            $select_nbuw = "SELECT B.ORDER_LIST,A.* 
-                                  FROM HXBX_KPI A 
-                                  LEFT JOIN TMP_CODE_MAP B 
-                                       ON A.BUSI_TYPE = B.CODE_TYPE 
+        }else{
+            $select_nbuw = "SELECT MAX(B.ORDER_LIST) AS ORDER_LIST,
+                                    MAX(C.EXE_NAME) AS EXE_NAME,
+                                    MAX(C.BUSI_TYPE_NAME) AS EXE_TYPE,
+                                    SUM(OLD_NUM) AS OLD_NUM,
+                                    SUM(NEW_NUM) AS NEW_NUM,
+                                    SUM(NEW_NUM)-SUM(OLD_NUM) AS NUM_DIFF,
+                                    TO_CHAR(ROUND(1-ABS(NVL(SUM(NEW_NUM),0)-NVL(SUM(OLD_NUM),0))/DECODE(SUM(OLD_NUM),0,1,SUM(OLD_NUM)),4)*100,'fm9999990.9999')||'%' AS NUM_SAMERATE,
+                                    SUM(OLD_AMOUNT) AS OLD_AMOUNT,
+                                    SUM(NEW_AMOUNT) AS NEW_AMOUNT,
+                                    SUM(NEW_AMOUNT)-SUM(OLD_AMOUNT) AS AMOUNT_DIFF,
+                                    TO_CHAR(ROUND(1-ABS(NVL(SUM(NEW_AMOUNT),0)-NVL(SUM(OLD_AMOUNT),0))/ABS(DECODE(SUM(OLD_AMOUNT),0,1,SUM(OLD_AMOUNT))),4)*100,'fm9999990.9999')||'%' AS AMOUNT_SAMERATE
+                                  FROM HXBX_KPI A
+                                  LEFT JOIN TMP_CODE_MAP B
+                                      ON A.BUSI_TYPE = B.CODE_TYPE
+                                  LEFT JOIN HXBX_KPI_CODES C
+                                    ON A.BUSI_TYPE = C.BUSI_TYPE
                                 WHERE A.BUSI_TYPE LIKE '%$BxOrganCode%' ".$sql_fix."
-                                ORDER BY B.ORDER_LIST";
+                                GROUP BY A.BUSI_TYPE 
+                                ORDER BY MAX(B.ORDER_LIST)";
             $result_rows = oci_parse($conn, $select_nbuw); // 配置SQL语句，执行SQL
             $result_all = $method->search_long($result_rows);
             Log::write($username.' 关键指标数据库查询SQL：'.$select_nbuw,'INFO');
@@ -382,7 +419,6 @@ class DayPostController extends Controller
                 $result[$exe['ORDER_LIST']]['FEE_DIFF'] = $exe['AMOUNT_DIFF'];
                 $result[$exe['ORDER_LIST']]['FEE_SAME_RADIO'] = $exe['AMOUNT_SAMERATE'];
             }
-        }else{
             $select_nbuw = "SELECT MAX(B.ORDER_LIST) AS ORDER_LIST,
                                     MAX(A.EXE_NAME) AS EXE_NAME,
                                     A.EXE_TYPE AS EXE_TYPE,
@@ -413,43 +449,6 @@ class DayPostController extends Controller
                 $result[$exe['ORDER_LIST']]['FEE_NEW_SUM'] = $exe['FEE_NEW_SUM'];
                 $result[$exe['ORDER_LIST']]['FEE_DIFF'] = $exe['FEE_DIFF'];
                 $result[$exe['ORDER_LIST']]['FEE_SAME_RADIO'] = $exe['FEE_SAME_RADIO'];
-            }
-            $select_nbuw = "SELECT B.ORDER_LIST,A.* 
-                                  FROM HXBX_KPI A 
-                                  LEFT JOIN TMP_CODE_MAP B 
-                                       ON A.BUSI_TYPE = B.CODE_TYPE 
-                                WHERE A.BUSI_TYPE LIKE '%$BxOrganCode%' ".$sql_fix."
-                                ORDER BY B.ORDER_LIST";
-//            $select_nbuw = "SELECT MAX(B.ORDER_LIST) AS ORDER_LIST,
-//                                    MAX(A.EXE_NAME) AS EXE_NAME,
-//                                    A.EXE_TYPE AS EXE_TYPE,
-//                                    SUM(OLD_NUM) AS OLD_NUM,
-//                                    SUM(NEW_NUM) AS NEW_NUM,
-//                                    SUM(NEW_NUM)-SUM(OLD_NUM) AS NUM_DIFF,
-//                                    TO_CHAR(ROUND(1-ABS(NVL(SUM(OLD_NUM),0)-NVL(SUM(NEW_NUM),0))/DECODE(SUM(OLD_NUM),0,1,SUM(OLD_NUM)),4)*100,'fm9999990.9999')||'%' AS NUM_SAME_RADIO,
-//                                    SUM(OLD_AMOUNT) AS OLD_AMOUNT,
-//                                    SUM(NEW_AMOUNT) AS NEW_AMOUNT,
-//                                    SUM(NEW_AMOUNT)-SUM(OLD_AMOUNT) AS AMOUNT_DIFF,
-//                                    TO_CHAR(ROUND(1-ABS(NVL(SUM(OLD_AMOUNT),0)-NVL(SUM(NEW_AMOUNT),0))/DECODE(SUM(OLD_AMOUNT),0,1,SUM(OLD_AMOUNT)),4)*100,'fm9999990.9999')||'%' AS FEE_SAME_RADIO
-//                                  FROM HXBX_KPI A
-//                                  LEFT JOIN TMP_CODE_MAP B
-//                                       ON A.BUSI_TYPE = B.CODE_TYPE
-//                                WHERE A.BUSI_TYPE LIKE '%$BxOrganCode%' ".$sql_fix."
-//                                GROUP BY A.EXE_TYPE ORDER BY B.ORDER_LIST";
-            $result_rows = oci_parse($conn, $select_nbuw); // 配置SQL语句，执行SQL
-            $result_all = $method->search_long($result_rows);
-            Log::write($username.' 关键指标数据库查询SQL：'.$select_nbuw,'INFO');
-            foreach($result_all AS $exe){
-                $result[$exe['ORDER_LIST']]['ZB_NAME'] = $exe['EXE_NAME'];
-                $result[$exe['ORDER_LIST']]['ZB_TYPE'] = $exe['EXE_TYPE'];
-                $result[$exe['ORDER_LIST']]['NUM_OLD_SUM'] = $exe['OLD_NUM'];
-                $result[$exe['ORDER_LIST']]['NUM_NEW_SUM'] = $exe['NEW_NUM'];
-                $result[$exe['ORDER_LIST']]['NUM_DIFF'] = $exe['NUM_DIFF'];
-                $result[$exe['ORDER_LIST']]['NUM_SAME_RADIO'] = $exe['NUM_SAMERATE'];
-                $result[$exe['ORDER_LIST']]['FEE_OLD_SUM'] = $exe['OLD_AMOUNT'];
-                $result[$exe['ORDER_LIST']]['FEE_NEW_SUM'] = $exe['NEW_AMOUNT'];
-                $result[$exe['ORDER_LIST']]['FEE_DIFF'] = $exe['AMOUNT_DIFF'];
-                $result[$exe['ORDER_LIST']]['FEE_SAME_RADIO'] = $exe['AMOUNT_SAMERATE'];
             }
         }
 //        for($i=0;$i<sizeof($result);$i++){
